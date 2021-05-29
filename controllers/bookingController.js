53a25415157f9686dfef79389d3eae2b070d7f31
piyushOP <pk930558@gmail.com>
2021-05-29 20:11:18 +0530

    Stripe updated

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,7 +2,6 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
-const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
@@ -58,7 +57,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
    const tour = session.client_reference_id;
    const user = (await User.findOne({ email: session.customer_email })).id;
-   const price = session.display_items[0].amount/100;
+   const price = session.amount_total / 100;
    await Booking.create({ tour, user, price });
 }
 
@@ -75,7 +74,7 @@ exports.webhookCheckout = (req, res, next) => {
       return res.status(400).send(`Webhook error: ${err.message}`);
    }
 
-   if(event.type === 'checkout.session.completed'){
+   if(event.type === "checkout.session.completed"){
       createBookingCheckout(event.data.object);
    }
 
