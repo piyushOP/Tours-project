1eaccb2f34ed45873f7c4e593c09887466971acb
piyushOP <pk930558@gmail.com>
2021-05-30 16:33:02 +0530

    css updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,31 +15,37 @@ function signToken(id) {
 
 
 // Create new Token , give response to the user..
-function createSendToken (user, statusCode, res, req) {
-  
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  
 
   // Saving jwt to cookie rather than local storage for security purpose..
   res.cookie('jwt', token, {
-    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
     httpOnly: true,
-    secure: req.secure || req.headers('x-forwarded-proto') === 'https'
-  }); 
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+  });
 
+  // Remove password from output
   user.password = undefined;
 
   res.status(statusCode).json({
-    status: "success",
+    status: 'success',
     token,
     data: {
-      user: user,
-    },
+      user
+    }
   });
-}
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
 
   const url = `${req.protocol}://${req.get('host')}/me`;
   await new Email(newUser, url).sendWelcome();
